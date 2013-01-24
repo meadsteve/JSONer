ab761a8564d3978371fef9718f5b97308d3527d4
meadsteve <meadsteve@gmail.com>
2013-01-24 18:16:45 +0000

    Very basic PHPUnit test of the JSONer class.

---
+++ UnitTests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+include_once "Hamcrest/Hamcrest.php";
+
+/**
+ * Creates an psr-0 style autoloader from the baseDir root.
+ * @param $baseDir
+ * @return callable
+ */
+function buildAutoload($baseDir)
+{
+    return function($className) use ($baseDir) {
+        $className = ltrim($className, '\\');
+        $fileName  = '';
+        if ($lastNsPos = strripos($className, '\\')) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+		$Path = $baseDir . DIRECTORY_SEPARATOR . $fileName;
+		if (file_exists($Path))
+        	require $Path;
+    };
+}
+
+spl_autoload_register(buildAutoload(__DIR__ . "/../../../"));
