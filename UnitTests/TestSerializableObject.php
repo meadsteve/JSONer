a66ba564947c416dbf778fc0e9acb2866bbbf434
meadsteve <meadsteve@gmail.com>
2013-01-24 19:00:27 +0000

    Adding tests to make sure the Serializable object interface is used correctly.

---
+++ UnitTests/TestSerializableObject.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: SteveB
+ * Date: 24/01/13
+ * Time: 18:47
+ *
+ */
+class TestSerializableObject implements \MeadSteve\JSONer\JsonSerializable{
+
+	/**
+	 * This function should return an object, array or primitive that
+	 * represents the implementing object. The returned value should be
+	 * encodable by json_encode.
+	 *
+	 * @return mixed
+	 */
+	public function jsonSerialize() {
+		$encodedObject = new \stdClass();
+		$encodedObject->keptProperty = $this->keptProperty;
+		return $encodedObject;
+	}
+
+	public $keptProperty;
+	public $lostProperty;
+}
